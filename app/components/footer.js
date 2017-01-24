6eccfcb20d30149c1d9aff48a102179a18d9349a
rbocobo <rbocobo@me.com>
2017-01-24 20:06:06 +0800

    Refactor to functional comp

--- app/components/footer.js
+++ app/components/footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Navbar} from 'react-bootstrap';
+import Style from "../css/app.css";
 
-const Footer = () => {return <Navbar fixedBottom>Footer</Navbar>}   
+const Footer = () => {return <Navbar fixedBottom className={Style.centerText}>Footer</Navbar>}   
 
 export default Footer
--- app/components/header.js
+++ app/components/header.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import {Navbar} from 'react-bootstrap';
+import Style from "../css/app.css";
 
-export default class Header extends React.Component{
-    render(){
-        return(
+const Header = () => {
+    return(
             <Navbar staticTop>
-                <Navbar.Header>
+                <Navbar.Header className={Style.centerText}>
                 Header
                 </Navbar.Header>
             </Navbar>
         );
-    }
-};
+}
+
+export default Header 
